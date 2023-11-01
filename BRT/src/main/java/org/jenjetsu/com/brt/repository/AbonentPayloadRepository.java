992eb30a4c65a2fe617afcd47f9fc04d0762e276
Stey <tsvetstas30@gmail.com>
2023-11-02 01:19:30 +1000

    Fixed cdr parser, cdrplus creator and entity serializers

--- BRT/src/main/java/org/jenjetsu/com/brt/repository/AbonentPayloadRepository.java
+++
@@ -1,9 +0,0 @@
-package org.jenjetsu.com.brt.repository;
-
-import org.jenjetsu.com.brt.entity.AbonentPayload;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AbonentPayloadRepository extends JpaRepository<AbonentPayload, Long> {
-}
--- BRT/src/main/java/org/jenjetsu/com/brt/repository/DeactivatedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package org.jenjetsu.com.brt.repository;
-
-import org.jenjetsu.com.brt.entity.DeactivatedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeactivatedTokenRepository extends JpaRepository<DeactivatedToken, String> {
-}
